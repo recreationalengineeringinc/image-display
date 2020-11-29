544db9bee4a3e59f560dfccec8ffc528cb0d2fd6
Terrence Wong <terrencewong223@gmail.com>
2020-11-28 23:01:37 -0800

    resized images

--- client/components/imageDisplay/singleImage.jsx
+++ client/components/imageDisplay/singleImage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+
 const SingleImage = (props) => (
-  <td onClick={() => (props.changeImage(props.index))}><div id='preview-display'><img id="image-preview" src={props.image} /></div></td>
+  <td onClick={() => (props.changeImage(props.index))}><div id='preview-display'><img id="image-preview" src={props.image.slice(0, 47) + 'resize' + props.image.slice(46, props.image.length)} /></div></td>
 );
 
 export default SingleImage;
