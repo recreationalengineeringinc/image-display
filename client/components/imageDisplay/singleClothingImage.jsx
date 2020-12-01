658b00a0b90bb4b27de8387304f8b5842d0f790a
Terrence Wong <terrencewong223@gmail.com>
2020-12-01 00:14:01 -0800

    updated to get dynamically resized photos

--- client/components/imageDisplay/singleClothingImage.jsx
+++ client/components/imageDisplay/singleClothingImage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SingleClothingImage = (props) => (
   <tr>
-    <td id='clothing-cells' onClick={() => (props.changeImage(props.index))}><img id="clothing-preview" src={props.image.slice(0, 47) + 'resize' + props.image.slice(46, props.image.length)} /></td>
+    <td id='clothing-cells' onClick={() => (props.changeImage(props.index))}><img id="clothing-preview" src={'https://d2j41hwrxgv80l.cloudfront.net/fit-in/150x150' + props.image.slice(46, props.image.length)} /></td>
     {/* <td id='clothing-cells' onClick={() => (props.changeImage(props.index))}><img id="clothing-preview" src={props.image} /></td> */}
   </tr>
 );
--- client/components/imageDisplay/singleImage.jsx
+++ client/components/imageDisplay/singleImage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const SingleImage = (props) => (
   <td onClick={() => (props.changeImage(props.index))}>
     <div id='preview-display'>
-      <img id="image-preview" src={props.image.slice(0, 47) + 'resize' + props.image.slice(46, props.image.length)} />
+      <img id="image-preview" src={'https://d2j41hwrxgv80l.cloudfront.net//fit-in/150x150' + props.image.slice(46, props.image.length)} />
       {/* <img id="image-preview" src={props.image} /> */}
     </div>
   </td>
